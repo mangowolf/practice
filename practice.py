ad0a881c53884076741ea635f05a3a46a196b05b
Jimmy Nguyen <jimmyz949@hotmail.com>
2017-05-23 21:50:49 -0700

    Type: Test Practice with binary searches.

--- practice.py
+++ practice.py
@@ -162,3 +162,29 @@ class Stack(object):
 
     def pop(self):
         return self.ll.delete_first()
+
+"""You're going to write a binary search function.
+You should use an iterative approach - meaning
+using loops.
+Your function should take two inputs:
+a Python list to search through, and the value
+you're searching for.
+Assume the list only has distinct elements,
+meaning there are no repeated values, and 
+elements are in a strictly increasing order.
+Return the index of value, or -1 if the value
+doesn't exist in the list."""
+
+def binary_search(input_array, value):
+    half = len(input_array)/2
+    while value > input_array[half]:
+        half += half/2
+        if input_array[half] == value:
+            return half
+    return -1
+
+test_list = [1,3,9,11,15,19,29]
+test_val1 = 25
+test_val2 = 15
+print binary_search(test_list, test_val1)
+print binary_search(test_list, test_val2)
