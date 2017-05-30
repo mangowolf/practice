7cc4b454e186efa2701bb1878bbb28bec5500f3c
Jimmy Nguyen <jimmyz949@hotmail.com>
2017-05-29 22:43:25 -0700

    Type: Test More practice with dictionaries.

--- practice.py
+++ practice.py
@@ -224,6 +224,23 @@ test = [21, 4, 1, 3, 9, 20, 25, 6, 21, 14]
 print quicksort(test)
 
 #Practice with dictionaries
+"""Time to play with Python dictionaries!
+You're going to work on a dictionary that
+stores cities by country and continent.
+One is done for you - the city of Mountain 
+View is in the USA, which is in North America.
+
+You need to add the cities listed below by
+modifying the structure.
+Then, you should print out the values specified
+by looking them up in the structure.
+
+Cities to add:
+Bangalore (India, Asia)
+Atlanta (USA, North America)
+Cairo (Egypt, Africa)
+Shanghai (China, Asia)"""
+
 locations = {'North America': {'USA': ['Mountain View']}}
 locations['North America']['USA'].append('Atlanta')
 locations['Asia'] = {'India': ['Bangalore']}
@@ -231,3 +248,19 @@ locations['Asia']['China'] = ['Shanghai']
 locations['Africa'] = {'Egypt': ['Cairo']}
 
 print locations
+"""Print the following (using "print").
+1. A list of all cities in the USA in
+alphabetic order."""
+
+for key in sorted(locations.iterkeys()):
+    print "%s: %s" % (key, locations[key])
+"""2. All cities in Asia, in alphabetic
+order, next to the name of the country.
+In your output, label each answer with a number
+so it looks like this:
+1
+American City
+American City
+2
+Asian City - Country
+Asian City - Country"""
