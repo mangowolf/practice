19c0ffbc5a9f58eadfc4f6d5bd6121e1b56f5036
Jimmy Nguyen <jimmyz949@hotmail.com>
2017-06-04 14:28:14 -0700

    Type: Test Practice with Hash Functions and Tables.

--- practice.py
+++ practice.py
@@ -357,3 +357,64 @@ for country in locations['Asia']:
 sortedCities = sorted(cities)
 for sCity in sortedCities:
     print sCity
+
+
+"""Write a HashTable class that stores strings
+in a hash table, where keys are calculated
+using the first two letters of the string."""
+
+class HashTable(object):
+    def __init__(self):
+        self.table = [None]*10000
+
+    def store(self, string):
+        """Input a string that's stored in 
+        the table."""
+        hashValue = self.calculate_hash_value(string)
+        if self.table[hashValue]:
+            self.table[hashValue].append(string)
+        else:
+            self.table[hashValue] = [string]
+
+    def lookup(self, string):
+        """Return the hash value if the
+        string is already in the table.
+        Return -1 otherwise."""
+        hashValue = self.calculate_hash_value(string)
+        if self.table[hashValue]:
+            return hashValue
+        else:
+            return -1
+
+    def calculate_hash_value(self, string):
+        """Helper function to calulate a
+        hash value from a string."""
+        if string:
+            twoLetter = string[:2]
+            hashValue = ""
+            for letter in twoLetter:
+                hashValue += str(ord(letter))
+            return int(hashValue)
+        else:
+            return -1
+
+# Setup
+hash_table = HashTable()
+
+# Test calculate_hash_value
+# Should be 8568
+print hash_table.calculate_hash_value('UDACITY')
+
+# Test lookup edge case
+# Should be -1
+print hash_table.lookup('UDACITY')
+
+# Test store
+hash_table.store('UDACITY')
+# Should be 8568
+print hash_table.lookup('UDACITY')
+
+# Test store edge case
+hash_table.store('UDACIOUS')
+# Should be 8568
+print hash_table.lookup('UDACIOUS')
