ab72862d99edaeca0dba3206459fee7bb821bdba
Jimmy Nguyen <jimmyz949@hotmail.com>
2017-06-08 07:27:04 -0700

    Type: Test Practice with Binary Search Trees.

--- practice.py
+++ practice.py
@@ -466,3 +466,47 @@ print tree.search(6)
 # Test print_tree
 # Should be 1-2-4-5-3
 print tree.print_tree()
+
+class Node(object):
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+class BST(object):
+    def __init__(self, root):
+        self.root = Node(root)
+
+    def insert(self, new_val):
+        self.insert_helper(self.root, new_val)
+    
+    def insert_helper(self, current, new_val):
+        if current < new_val:
+            if current.right:
+                self.insert_helper(current.right, new_val)
+            else:
+                current.right = Node(new_val)
+        else:
+            if current > new_val:
+                if current.left:
+                    self.insert_helper(current.left, new_val)
+                else:
+                    current.left = Node(new_val)
+
+    def search(self, find_val):
+        return False
+    
+# Set up tree
+tree = BST(4)
+
+# Insert elements
+tree.insert(2)
+tree.insert(1)
+tree.insert(3)
+tree.insert(5)
+
+# Check search
+# Should be True
+print tree.search(4)
+# Should be False
+print tree.search(6)
