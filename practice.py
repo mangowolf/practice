fc88ef97d58191d3f120d8b74d6aec34c2a3eaf4
Jimmy Nguyen <jimmyz949@hotmail.com>
2017-06-11 15:08:10 -0700

    Type: Test More practice with adjacency lists.

--- practice.py
+++ practice.py
@@ -586,16 +586,22 @@ class Graph(object):
         Each section in the list will store a list
         of tuples that looks like this:
         (To Node, Edge Value)"""
-        edgeList = self.get_edge_list()
-        nodeArray = self.nodes
-        edgeAdjArray = [None]
-        for idx, edge in enumerate(edgeList):
-            nodeVal = edge[1]
-            for node in nodeArray:
-                if node.value == nodeVal:
-                    tuple = (edge[2],edge[0])
-                    edgeAdjArray.append(tuple)
-        return edgeAdjArray
+        max_index = self.find_max_index()
+        adjacency_list = [None] * (max_index + 1)
+        for edge_object in self.edges:
+            if adjacency_list[edge_object.node_from.value]:
+                adjacency_list[edge_object.node_from.value].append((edge_object.node_to.value, edge_object.value))
+            else:
+                adjacency_list[edge_object.node_from.value] = [(edge_object.node_to.value, edge_object.value)]
+        return adjacency_list
+    
+    def find_max_index(self):
+        max_index = -1
+        if len(self.nodes):
+            for node in self.nodes:
+                if node.value > max_index:
+                    max_index = node.value
+        return max_index
         
     def get_adjacency_matrix(self):
         """Return a matrix, or 2D list.
