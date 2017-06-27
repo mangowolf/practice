ceef32ca3e644f2b0d1d14006520906cf53b32ae
Jimmy Nguyen <jimmyz949@hotmail.com>
2017-06-26 21:35:35 -0700

    Type: Test Completed question 3 and Technical Interview Questions.

--- question3.py
+++ question3.py
@@ -5,97 +5,73 @@ Given an undirected graph G, find the minimum spanning tree within G. A minimum 
  'C': [('B', 5)]}
 Vertices are represented as unique strings. The function definition should be question3(G)"""
 
-class Node(object):
-	def __init__(self, value):
-		self.value = value
-		self.edges = []
+parent = dict()
+rank = dict()
 
-class Edge(object):
-	def __init__(self, value, node_from, node_to):
-		self.value = value
-		self.node_from = node_from
-		self.node_to = node_to
+#Function to make a tree subset
+def make_set(vertice):
+    parent[vertice] = vertice
+    rank[vertice] = 0
 
-class Graph(object):
-	def __init__(self, nodes=[], edges=[]):
-		self.nodes = nodes
-		self.edges = edges
+#Function to find the root of the path
+def find(vertice):
+    if parent[vertice] != vertice:
+        parent[vertice] = find(parent[vertice])
+    return parent[vertice]
 
-class UnionFind:
-    """Union-find data structure.
+#Function to combine tree subsets
+def union(v1, v2):
+    r1 = find(v1)
+    r2 = find(v2)
+    if r1 != r2:
+        if rank[r1] > rank[r2]:
+            parent[r2] = r1
+        else:
+            parent[r1] = r2
+            if rank[r1] == rank[r2]: 
+                rank[r2] += 1
 
-    Each unionFind instance X maintains a family of disjoint sets of
-    hashable objects, supporting the following two methods:
+#Function to create the minimum spanning tree using path compression and Kruskals algorithm
+def kruskalAlgorithm(graph):
+    for vertice in graph['vertices']:
+        make_set(vertice)
 
-    - X[item] returns a name for the set containing the given item.
-      Each set is named by an arbitrarily-chosen one of its members; as
-      long as the set remains unchanged it will keep the same name. If
-      the item is not yet part of a set in X, a new singleton set is
-      created for it.
+    minimumSpanningTree = []
+    edges = list(graph['edges'])
+    edges.sort()
+    for edge in edges:
+        w, v1, v2 = edge
+        if find(v1) != find(v2):
+            union(v1,v2)
+            minimumSpanningTree.append(edge)
 
-    - X.union(item1, item2, ...) merges the sets containing each item
-      into a single larger set.  If any item is not yet part of a set
-      in X, it is added to X as one of the members of the merged set.
-    """
+    return minimumSpanningTree
 
-    def __init__(self):
-        """Create a new empty union-find structure."""
-        self.weights = {}
-        self.parents = {}
-
-    def __getitem__(self, object):
-        """Find and return the name of the set containing the object."""
-
-        # check for previously unknown object
-        if object not in self.parents:
-            self.parents[object] = object
-            self.weights[object] = 1
-            return object
-
-        # find path of objects leading to the root
-        path = [object]
-        root = self.parents[object]
-        while root != path[-1]:
-            path.append(root)
-            root = self.parents[root]
-
-        # compress the path and return
-        for ancestor in path:
-            self.parents[ancestor] = root
-        return root
-        
-    def __iter__(self):
-        """Iterate through all items ever found or unioned by this structure."""
-        return iter(self.parents)
-
-    def union(self, *objects):
-        """Find the sets containing the objects and merge them all."""
-        roots = [self[x] for x in objects]
-        heaviest = max([(self.weights[r],r) for r in roots])[1]
-        for r in roots:
-            if r != heaviest:
-                self.weights[heaviest] += self.weights[r]
-                self.parents[r] = heaviest
-
+#Function taking in adjacency list graph and returning minimum spanning tree as an adjacency matrix
 def question3(G):
-    subtrees = UnionFind()
-    tree = []
-    print G['A'][0][1]
-    for u,v in sorted((G[u][0][1],u,v) for u in G for v in G[u]):
-        if subtrees[u] != subtrees[v[0]]:
-            tree.append((u,v))
-            subtrees.union(u,v)
-    """
-    for W,u,v in sorted((G[u][v],u,v) for u in G for v in G[u]):
-        if subtrees[u] != subtrees[v]:
-            tree.append((u,v))
-            subtrees.union(u,v)
-    return tree
-    G = {0:{1:11,2:13,3:12},1:{0:11,3:14},2:{0:13,3:10},3:{0:12,1:14,2:10}}
-    """
+
+    graph = {
+        'vertices': G.keys(),
+        'edges': []
+    }
+
+    for v in G:
+        for u in xrange(0,len(G[v])):
+            edge = (G[v][u][1],v,G[v][u][0])
+            graph['edges'].append(edge)
+    
+    mst = kruskalAlgorithm(graph)
+
+    adjList = {}
+    for i in xrange(0,len(mst)):
+        adjList[mst[i][1]] = [mst[i][2],mst[i][0]]
+
+    return adjList
 
 G = {'A': [('B', 2)],
  'B': [('A', 2), ('C', 5)], 
  'C': [('B', 5)]}
 
 print question3(G)
+
+#Efficiency O(n^2)
--- solutions.py
+++ solutions.py
@@ -28,14 +28,14 @@ def question(s,t):
 	else:
 		return False
 
-"""print question('udacity', 'ad')
+#TEST CASES
+print question('udacity', 'ad')
 print question('1up2', 21)
 print question('\delete', 't\ele')
 print question('Nordic', 'Ndd')
 print question('Nordic', 'ndd')
 print question('Nordic', 'Nd')
 print question('Nordic', 'nd')
-"""
 
 #Efficiency
 #O(n^2)
@@ -74,7 +74,7 @@ def question2(a):
 	palindrome = "".join(longPal)
 	return palindrome
 
-
+#TEST CASES
 print question2('redivider')
 #should print out redivider
 print question2('forgeeksskeegfor')
@@ -83,6 +83,106 @@ print question2('forgeeksskeegfor')
 #Efficiency
 #O(n)
 
+"""QUESTION 3
+Given an undirected graph G, find the minimum spanning tree within G. A minimum spanning tree connects all vertices in a graph with the smallest possible total weight of edges. Your function should take in and return an adjacency list structured like this:
+{'A': [('B', 2)],
+ 'B': [('A', 2), ('C', 5)], 
+ 'C': [('B', 5)]}
+Vertices are represented as unique strings. The function definition should be question3(G)"""
+
+parent = dict()
+rank = dict()
+
+#Function to make a tree subset
+def make_set(vertice):
+    parent[vertice] = vertice
+    rank[vertice] = 0
+
+#Function to find the root of the path
+def find(vertice):
+    if parent[vertice] != vertice:
+        parent[vertice] = find(parent[vertice])
+    return parent[vertice]
+
+#Function to combine tree subsets
+def union(v1, v2):
+    r1 = find(v1)
+    r2 = find(v2)
+    if r1 != r2:
+        if rank[r1] > rank[r2]:
+            parent[r2] = r1
+        else:
+            parent[r1] = r2
+            if rank[r1] == rank[r2]: 
+                rank[r2] += 1
+
+#Function to create the minimum spanning tree using path compression and Kruskals algorithm
+def kruskalAlgorithm(graph):
+    for vertice in graph['vertices']:
+        make_set(vertice)
+
+    minimumSpanningTree = []
+    edges = list(graph['edges'])
+    edges.sort()
+    for edge in edges:
+        w, v1, v2 = edge
+        if find(v1) != find(v2):
+            union(v1,v2)
+            minimumSpanningTree.append(edge)
+
+    return minimumSpanningTree
+
+#Function taking in adjacency list graph and returning minimum spanning tree as an adjacency matrix
+def question3(G):
+
+    graph = {
+        'vertices': G.keys(),
+        'edges': []
+    }
+
+    for v in G:
+        for u in xrange(0,len(G[v])):
+            edge = (G[v][u][1],v,G[v][u][0])
+            graph['edges'].append(edge)
+    
+    mst = kruskalAlgorithm(graph)
+
+    adjList = {}
+    for i in xrange(0,len(mst)):
+        adjList[mst[i][1]] = [mst[i][2],mst[i][0]]
+
+    return adjList
+
+#TEST CASE
+G = {'A': [('B', 2)],
+ 'B': [('A', 2), ('C', 5)], 
+ 'C': [('B', 5)]}
+
+print question3(G)
+
+#Efficiency O(n^2)
+
+
+"""QUESTION 4
+Find the least common ancestor between two nodes on a binary search tree. 
+The least common ancestor is the farthest node from the root that is an ancestor of both nodes. 
+For example, the root is a common ancestor of all nodes on the tree, but if both nodes are descendents 
+of the root's left child, then that left child might be the lowest common ancestor. You can assume that 
+both nodes are in the tree, and the tree itself adheres to all BST properties. The function definition 
+should look like question4(T, r, n1, n2), where T is the tree represented as a matrix, where the index 
+of the list is equal to the integer stored in that node and a 1 represents a child node, r is a 
+non-negative integer representing the root, and n1and n2 are non-negative integers representing the 
+two nodes in no particular order. For example, one test case might be
+question4([[0, 1, 0, 0, 0],
+           [0, 0, 0, 0, 0],
+           [0, 0, 0, 0, 0],
+           [1, 0, 0, 0, 1],
+           [0, 0, 0, 0, 0]],
+          3,
+          1,
+          4)
+and the answer would be 3.
+"""
 
 class Node(object):
 	def __init__(self, value):
@@ -148,6 +248,7 @@ def question4(T,r,n1,n2):
 	commonAncestor = lca(parent,n1,n2)
 	return commonAncestor.value
 
+#TEST CASE
 print question4([[0, 1, 0, 0, 0],
            [0, 0, 0, 0, 0],
            [0, 0, 0, 0, 0],
@@ -242,6 +343,7 @@ LinkedListObj.append(nArray[3])
 LinkedListObj.append(nArray[4])
 LinkedListObj.append(nArray[5])
 
+#TEST CASE
 print LinkedListObj.question5(nArray[1],3)
 
 """Efficiency of question5() is:
