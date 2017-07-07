55f8c1c629bbc0ec2fab5640899283ccd88f2583
Jimmy Nguyen <jimmyz949@hotmail.com>
2017-07-06 22:00:04 -0700

    Type: Test More practice with quicksort.

--- Quicksort.py
+++ Quicksort.py
@@ -1,30 +1,40 @@
 """QuickSort"""
 
-def partition(arr,low, high):
+def partition(arr, low, high):
 
-	#Set pivot, using right most element.
+	#Set pivot point choosing further value to the right of the partition
 	pivot = arr[high]
 
-	#Index of smaller element
+	#Set smaller index element
 	i = (low - 1)
-
-	#compare values and swap if less than pivot
+	print i
+	#Check the current element against the pivot point
 	for j in range(low, high):
+		
+		#if the current element is less than the pivot value
+		#increment the smaller index value, and swap current value
+		#with the smaller index value
 		if arr[j] <= pivot:
+			print j
 			i = i+1
+			print i
 			arr[i],arr[j] = arr[j],arr[i]
-	
-	arr[i+1],arr[high] = arr[high], arr[i+1]
+
+	arr[i+1],arr[high] = arr[high],arr[i+1]
 	return (i+1)
 
 def quicksort(arr, low, high):
 	if low < high:
-		pi = partition(arr, low, high)
 
-		quicksort(arr, low, pi - 1)
-		quicksort(arr, pi + 1, high)
+		#partition the array
+		pi = partition(arr,low,high)
+
+		#quicksort values to the right and left of the partition
+		quicksort(arr,low,pi-1)
+		quicksort(arr,pi+1,high)
 
 arr = [10, 7, 8, 9, 1, 5]
+
 n = len(arr)
 quicksort(arr,0,n-1)
 
